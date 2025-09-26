3a6fb1827e5369d1c525fc59acfb8c90667369ee
Katie Barnett <katiebarnett5@gmail.com>
2025-09-26 16:22:00 +1000

    Exercise 2 - in progress - stuck on how to combine scenes

--- app/src/main/java/com/example/nav3recipes/uxr/Exercise1Activity.kt
+++ app/src/main/java/com/example/nav3recipes/uxr/Exercise1Activity.kt
@@ -24,18 +24,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.layout.PaneScaffoldDirective
 import androidx.compose.material3.adaptive.layout.calculatePaneScaffoldDirective
 import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
 import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
+import androidx.navigation3.ui.Scene
+import androidx.navigation3.ui.SceneStrategy
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
 import com.example.nav3recipes.content.ContentBase
 import com.example.nav3recipes.ui.setEdgeToEdgeConfig
 import com.example.nav3recipes.ui.theme.Grey90
@@ -55,6 +61,40 @@ data class ConversationDetail(val id: Int) : NavKey {
         get() = colors[id % colors.size]
 }
 
+
+data class TitledSinglePaneScene<T : Any>(
+    override val key: Any,
+    val entry: NavEntry<T>,
+    override val previousEntries: List<NavEntry<T>>,
+) : Scene<T> {
+    override val entries: List<NavEntry<T>> = listOf(entry)
+    override val content: @Composable () -> Unit = { entry.Content() }
+}
+
+class TitledSinglePaneSceneStrategy<T : Any> : SceneStrategy<T> {
+    @Composable
+    override fun calculateScene(entries: List<NavEntry<T>>, onBack: (Int) -> Unit): Scene<T>? {
+
+
+        val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+
+        // Return null if the current window size is larger than 600dp - so we show the two pane without the title
+        // WIDTH_DP_MEDIUM_LOWER_BOUND (600dp).
+        if (windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+            return null
+        }
+
+
+        return TitledSinglePaneScene(
+            key = entries.last().contentKey,
+            entry = entries.last(),
+            previousEntries = entries.dropLast(1)
+        )
+
+    }
+}
+
+
 class Exercise1Activity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3AdaptiveApi::class)
@@ -71,11 +111,12 @@ class Exercise1Activity : ComponentActivity() {
             }
             val listDetailStrategy = rememberListDetailSceneStrategy<NavKey>(directive = directive)
 
+
             Scaffold { paddingValues ->
                 NavDisplay(
                     backStack = backStack,
                     onBack = { backStack.removeLastOrNull() },
-                    sceneStrategy = listDetailStrategy,
+                    sceneStrategy = TitledSinglePaneSceneStrategy<Any>() then listDetailStrategy,
                     modifier = Modifier.padding(paddingValues),
                     entryProvider = entryProvider {
                         entry<ConversationList>(
